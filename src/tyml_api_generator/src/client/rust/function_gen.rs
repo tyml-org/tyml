ff1342e77543eb3fdd6fb6ff9fb642a2ae884dee
bea4dev <bea0224@outlook.jp>
2025-08-31 20:11:38 +0900

    impl rust client

--- src/tyml_api_generator/src/client/rust/function_gen.rs
+++ src/tyml_api_generator/src/client/rust/function_gen.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-use tyml_core::Tyml;
+use tyml_core::{Tyml, tyml_type::types::FunctionKind};
 
 use crate::{GeneratorSettings, general::rust::generate_type_for_rust, name::NameContext};
 
@@ -28,6 +28,14 @@ fn generate_struct(tyml: &Tyml) -> String {
     let mut type_def = String::new();
     let mut name_context = NameContext::new();
 
+    type_def += "
+pub enum ThrowsOrOtherError<T> {
+    Throws(T),
+    Other(reqwest::Error),
+}
+
+";
+
     for interface in tyml.interfaces().iter() {
         source += format!("pub struct {} {{\n", interface.original_name).as_str();
         source += "    pub url: String,\n";
@@ -40,16 +48,10 @@ fn generate_struct(tyml: &Tyml) -> String {
 
             let mut arguments = Vec::new();
 
-            if let Some(claim) = &function.claim_argument_info {
-                arguments.push(format!(
-                    "__claim: {}",
-                    generate_type_for_rust(
-                        &claim.ty,
-                        &mut type_def,
-                        &mut name_context,
-                        tyml.named_type_map()
-                    )
-                ));
+            arguments.push("&self".to_string());
+
+            if let Some(_) = &function.claim_argument_info {
+                arguments.push("__token: impl Into<String>".to_string());
             }
 
             if let Some(body) = &function.body_argument_info {
@@ -81,11 +83,11 @@ fn generate_struct(tyml: &Tyml) -> String {
 
             match (&function.return_info, &function.throws_type) {
                 (None, None) => {
-                    source += " {\n";
+                    source += " -> Result<(), ThrowsOrOtherError<()>> {\n";
                 }
                 (None, Some(throws_type)) => {
                     source += format!(
-                        " -> Result<(), {}> {{\n",
+                        " -> Result<(), ThrowsOrOtherError<{}>> {{\n",
                         generate_type_for_rust(
                             throws_type,
                             &mut type_def,
@@ -97,7 +99,7 @@ fn generate_struct(tyml: &Tyml) -> String {
                 }
                 (Some(return_info), None) => {
                     source += format!(
-                        " -> {} {{\n",
+                        " -> Result<{}, ThrowsOrOtherError<()>> {{\n",
                         generate_type_for_rust(
                             &return_info.ty,
                             &mut type_def,
@@ -109,7 +111,7 @@ fn generate_struct(tyml: &Tyml) -> String {
                 }
                 (Some(return_info), Some(throws_type)) => {
                     source += format!(
-                        " -> Result<{}, {}> {{\n",
+                        " -> Result<{}, ThrowsOrOtherError<{}>> {{\n",
                         generate_type_for_rust(
                             &return_info.ty,
                             &mut type_def,
@@ -125,6 +127,55 @@ fn generate_struct(tyml: &Tyml) -> String {
                     )
                     .as_str();
                 }
+            }
+
+            source += "        let client = reqwest::Client::new();\n";
+
+            let method = match function.kind {
+                FunctionKind::GET => "get",
+                FunctionKind::PUT => "put",
+                FunctionKind::POST => "post",
+                FunctionKind::PATCH => "patch",
+                FunctionKind::DELETE => "delete",
+            };
+
+            source += format!(
+                r#"        let mut request = client.{}(format!("{{}}/{}/{}", &self.url))"#,
+                method, &interface.name.value, &function.name.value
+            )
+            .as_str();
+            source += "\n";
+
+            if let Some(_) = &function.claim_argument_info {
+                source += "        request = request.bearer_auth(__token);\n";
+            }
+
+            let query = function
+                .arguments
+                .iter()
+                .map(|argument| argument.name.value.as_ref())
+                .map(|name| format!(r#"("{}", serde_json::to_string({}).unwrap())"#, name, name))
+                .collect::<Vec<_>>()
+                .join(", ");
+
+            source += format!("        request = request.query(&[{}]);\n", query).as_str();
+
+            source += r#"        request = request.header("Accept", "application/json");"#;
+            source += "\n";
+
+            if let Some(_) = &function.body_argument_info {
+                source += "        request = request.json(__body);\n";
+            }
+
+            source += "        let response = request.send().await.map_err(|error| ThrowsOrOtherError::Other(error))?;\n";
+
+            match (&function.return_info, &function.throws_type) {
+                (None, None) => {
+                    source += "";
+                }
+                (None, Some(_)) => todo!(),
+                (Some(_), None) => todo!(),
+                (Some(_), Some(_)) => todo!(),
             }
         }
     }
